b026b24acb0e5fc116d58f6289499bfbebf69671
solitdio079 <solitdio079@gmail.com>
2025-05-02 10:11:19 +0300

    Initialize Project
    I just created the HTML, CSS and JavaScript files needed for this Project
    I also created the four basic functions for the four basic operations.

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    return a + b
+}
+function substract(a,b){
+    return a -b
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
+
+console.log(add(3,5))
+console.log(substract(3,5))
+console.log(multiply(3,5))
+console.log(divide(3,5))
