7e1e6c39ce501e8c05c9d46c46885738d1974a88
solitdio079 <solitdio079@gmail.com>
2025-05-02 14:37:01 +0300

    Complete the functionalities

--- script.js
+++ script.js
@@ -1,17 +1,118 @@
+const displayEl = document.querySelector("#display")
+const btnElements = document.querySelector("#buttons")
+const decimalPointEl = document.querySelector("#decimalPoint")
+const prev = document.querySelector("#prev")
+
 function add(a,b){
     return a + b
 }
-function substract(a,b){
+function subtract(a,b){
     return a -b
 }
 function multiply(a,b){
     return a*b
 }
 function divide(a,b){
+    if(b === 0){
+        return "Division by 0 is not allowed!"
+    }
     return a/b
 }
 
-console.log(add(3,5))
-console.log(substract(3,5))
-console.log(multiply(3,5))
-console.log(divide(3,5))
+let num1 = 0
+let num2 = 0
+let ans
+let operations = []
+
+
+
+function operate(num1,num2,operation){
+      if(operation === "+"){
+        return add(num1,num2)
+      }else if(operation === "-"){
+        return subtract(num1,num2)
+      }else if(operation === "/"){
+        return divide(num1,num2)
+      }else if(operation === "*" || operation === "x"){
+        return multiply(num1,num2)
+      }else{
+        return "OPERATOR İNCORRECT ERR"
+      }
+}
+
+let clearForNextOperation = false
+
+
+
+// Event Listeners
+btnElements.addEventListener("click", (e)=> {
+
+    if(e.target.classList.contains("operator")){
+        if(operations.length === 1){
+          doOperation()
+        }
+        decimalPointEl.disabled = false
+        operations.push(e.target.textContent)
+    }
+
+   
+
+
+    if(e.target.classList.contains("btn")){
+
+        if(e.target.id === "equal"){
+            doOperation()
+            decimalPointEl.disabled = false
+            clearForNextOperation = true
+            
+        }
+        
+        else if(e.target.id === "delete"){
+            displayEl.textContent  = displayEl.textContent.slice(0, displayEl.textContent.length-1)
+        }else if(e.target.id === "clear"){
+            ans = 0
+            displayEl.textContent = ""
+        }else if (e.target.id === "decimalPoint"){
+            decimalPointEl.disabled = true
+
+            if(displayEl.textContent){
+                console.log("Display content", displayEl.textContent === "")
+                displayEl.textContent += e.target.textContent
+            }
+          
+            //displayEl.textContent += e.target.textContent
+            
+        }
+        else{
+            if(clearForNextOperation){
+                displayEl.textContent = e.target.textContent
+                clearForNextOperation = false
+            }else{
+                displayEl.textContent += e.target.textContent
+            }
+           
+        }
+       
+    }
+   
+})
+
+
+function doOperation(){
+    if(operations.length > 0){
+        const splitDisplayed =  displayEl.textContent.split(operations[0])
+        num1 = +splitDisplayed[0] || ans
+        num2= +splitDisplayed[1]
+        if(num2){
+            ans = operate(num1,num2,operations[0])
+            displayEl.textContent= ans
+
+        }
+        else{
+            displayEl.textContent= num1
+        }
+       
+        operations=[]
+       
+    }
+}
